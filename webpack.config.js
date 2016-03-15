084cdd585e772076ffa35dd294a03a63b19f2e20
djacquel <github@pygmeeweb.com>
2016-03-15 17:14:08 +0100

    working code after http://survivejs.com/webpack_react/webpack_and_react/

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,6 @@ const webpack = require('webpack');
 const path = require('path');
 const merge = require('webpack-merge');
 
-const NpmInstallPlugin = require('npm-install-webpack-plugin');
-
 const TARGET = process.env.npm_lifecycle_event;
 
 const PATHS = {
@@ -11,11 +9,20 @@ const PATHS = {
   build: path.join(__dirname, 'build')
 };
 
+
+process.env.BABEL_ENV = TARGET;
+
 const common = {
   // Entry accepts a path or an object of entries. We'll be using the
   // latter form given it's convenient with more complex configurations.
   entry: {
     app: PATHS.app
+  },
+  // Add resolve.extensions.
+  // '' is needed to allow imports without an extension.
+  // Note the .'s before extensions as it will fail to match without!!!
+  resolve: {
+    extensions: ['', '.js', '.jsx']
   },
   output: {
     path: PATHS.build,
@@ -28,6 +35,17 @@ const common = {
         test: /\.css$/,
         loaders: ['style', 'css'],
         // Include accepts either a path or an array of paths.
+        include: PATHS.app
+      },
+      // Set up jsx. This accepts js too thanks to RegExp
+      {
+        test: /\.jsx?$/,
+        // Enable caching for improved performance during development
+        // It uses default OS directory by default. If you need something
+        // more custom, pass a path to it. I.e., babel?cacheDirectory=<path>
+        loaders: ['babel?cacheDirectory'],
+        // Parse only app files! Without this it will go through entire project.
+        // In addition to being slow, that will most likely result in an error.
         include: PATHS.app
       }
     ]
@@ -66,10 +84,7 @@ if(TARGET === 'start' || !TARGET) {
       port: process.env.PORT
     },
     plugins: [
-      new webpack.HotModuleReplacementPlugin(),
-      new NpmInstallPlugin({
-        save: true // --save
-      })
+      new webpack.HotModuleReplacementPlugin()
     ]
   });
 
