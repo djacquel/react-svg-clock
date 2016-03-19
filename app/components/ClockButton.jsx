a175306a504839170acd3a86f14edf1ba24562ec
djacquel <github@pygmeeweb.com>
2016-03-19 14:39:37 +0100

    clock button styling + refactoring

--- app/components/ClockButton.jsx
+++ app/components/ClockButton.jsx
@@ -3,12 +3,12 @@ import React, { PropTypes } from 'react';
 const ClockButton = ({text, onClick, className, clockRadius, order}) => {
 
     var yButton = clockRadius*2 + 10 + 30*(order-1);
-    var ytext = yButton + 12;
+    var ytext = yButton + 15;
 
     return (
-        <g>
-            <rect x='0' y={yButton} width={clockRadius*2} rx="5" height='20' className="button {className}" onClick={onClick} />
-            <text x='10' y={ytext} children={text} />
+        <g className='buttonGroup' onClick={onClick} >
+            <rect x='0' y={yButton} width={clockRadius*2} rx="5" height='20' className={"button " + className} />
+            <text x={clockRadius} textAnchor='middle' y={ytext} className="buttonText" children={text} />
         </g>
     );
 }
