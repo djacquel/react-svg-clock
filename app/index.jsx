c1ac21b218df70d266c49760c9ecf8c0328b709a
djacquel <github@pygmeeweb.com>
2016-03-23 15:39:17 +0100

    added router

--- app/index.jsx
+++ app/index.jsx
@@ -3,6 +3,16 @@ import './main.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Clock from './components/Clock.jsx';
+import { Router, Route, hashHistory } from 'react-router';
 
-ReactDOM.render(<Clock />, document.getElementById('app'));
+import Clock from './components/Clock.jsx';
+import Countdown from './components/Countdown.jsx';
+import About from './components/About.jsx';
+
+ReactDOM.render(
+    <Router history={hashHistory}>
+        <Route path="/" component={Clock}/>
+        <Route path="/about" component={About}/>
+        <Route path="/countdown" component={Countdown}/>
+    </Router>, document.getElementById('app')
+);
