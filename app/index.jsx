98b69bb3714a221b0d76924d6053e09e36dc6d59
djacquel <github@pygmeeweb.com>
2016-03-27 11:45:45 +0200

    added a index and fallback routes to clock

--- app/index.jsx
+++ app/index.jsx
@@ -2,7 +2,7 @@ import './clock.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 
 import App from './components/App.jsx';
 import Clock from './components/Clock.jsx';
@@ -13,7 +13,7 @@ import About from './components/About.jsx';
 ReactDOM.render(
     <Router history={hashHistory}>
         <Route path="/" component={App}>
-            <Route path="/clock" component={Clock}/>
+            <IndexRoute component={Clock} />
             <Route path="/chrono" component={Chrono}/>
             <Route path="/countdown" component={Countdown}/>
             <Route path="/about" component={About}/>
